6c86a4667181423313a29abed345fe2db99f74df
Mohammad Abouchama <mohammadabouchama@gmail.com>
2019-07-09 11:02:18 +0300

    Remove test project

--- example_project/posts/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-
-from .views import PostListView, PostDetailView
-
-urlpatterns = [
-    url(r'(?P<pk>\d+)/$', PostDetailView.as_view(), name='detail'),
-    url(r'$', PostListView.as_view(), name='list'),
-]
